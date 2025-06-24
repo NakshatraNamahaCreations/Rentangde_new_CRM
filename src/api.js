f677786212f60ce7d0265846658cf0f3106948b4
NakshatraNamahaCreations <ops.nnc@gmail.com>
2025-06-24 14:37:26 +0530

    api.js update

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-// export const ApiURL = "https://api.rentangadi.in/api";
-// export const ImageApiURL = "https://api.rentangadi.in";
+export const ApiURL = "https://api.rentangadi.in/api";
+export const ImageApiURL = "https://api.rentangadi.in";
 
-export const ApiURL = "http://192.168.1.199:8000/api";
-export const ImageApiURL = "http://192.168.1.199:8000";
+// export const ApiURL = "http://192.168.1.199:8000/api";
+// export const ImageApiURL = "http://192.168.1.199:8000";
